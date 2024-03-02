93369db22cb369b41bfd7ffd4ba2e4c3e260ae8a
Tastan Kodersen <andmarknu@hotmail.com>
2024-03-02 16:50:19 +0100

    02.03.2024 16:50

--- reactblogsite/src/Components/Server/Database/Comments.ts
+++ reactblogsite/src/Components/Server/Database/Comments.ts
@@ -1,9 +1,9 @@
-import { Comment } from "../../../Types/Types"
+ import { Comment } from "../../../Types/Types"
 
 export let Comments: Comment[] = [
     {
         userName: "",
-        comment: "",
+        comment: "My first comment to a post",
         dateTimeStamp: new Date().toLocaleString(),
     },
 ]
--- reactblogsite/src/Components/Site/Footer/FooterWrapper.tsx
+++ reactblogsite/src/Components/Site/Footer/FooterWrapper.tsx
@@ -3,15 +3,6 @@ import { useUserContext } from '../../../Hooks/useUserContext'
 export const FooterWrapper = () => {
     const {user} = useUserContext();
 
-    // let posts;
-    // if(user !== undefined){
-    // posts = user.blog?.map((element) => 
-    //     <div className='posts'>
-    //         <h1>{element.blogTitle}</h1>
-            
-    //     </div>
-    // )
-    // }
     return (
         <div className="footerWrapper">
             

a7704528ed5f362b6f2278f495f43f2e7ef844fa
Andreas <andmarknu@hotmail.com>
2024-03-15 09:26:27 +0100

    15.03.2024 09:26

--- reactblogsite/src/Components/Site/Header/UserSidePanel/UserSidePanel.tsx
+++ reactblogsite/src/Components/Site/Header/UserSidePanel/UserSidePanel.tsx
@@ -1,14 +1,32 @@
 import './UserSidePanel.scss'
 import { useUserContext } from '../../../../Hooks/Context/useContext/useUserContext'
+import { ChangeEvent, useRef, useState } from 'react'
 export const UserSidePanel = () => {
-    const openBurgerMenu = () => {}
-    const closeBurgerMenu = () => {}
+    
+    const [ openMenu, setOpenMenu ] = useState<boolean>(false);
+    const { user } = useUserContext();
+    let textEmail = useRef<string>()
+    const textinputEmail = (x: ChangeEvent<HTMLInputElement>) => {
+        textEmail.current = x.target.value;
+    }
+    const openBurgerMenu = () => {
+        setOpenMenu(!openMenu)
+    }
+    const closeBurgerMenu = () => {
+        setOpenMenu(!openMenu)
+    }
+
+    const profile = <div>
+        <label htmlFor='email'>email</label>
+        <input id="email" type="text"  onChange={textinputEmail}/>
+    </div>
     return (
         <div>
-            <div id="openHeaderburgerMenu" className="openHeaderburgerMenu" onClick={()=>{}}>☰</div>
-            <div id="headerBurgerMenu" className="headerBurgerMenu">
-                <div id="closeHeaderBurgerMenu" className="closeHeaderBurgerMenu" onClick={()=>{}}>X</div>
-            </div>
+            {!openMenu && <div id="openburgerMenu" className="openburgerMenu" onClick={openBurgerMenu}>☰</div>}
+            {openMenu && <div id="burgerMenu" className="burgerMenu">
+                {profile}
+                <div id="closeBurgerMenu" className="closeBurgerMenu" onClick={closeBurgerMenu}>X</div>
+            </div>}
         </div>
     )
 }

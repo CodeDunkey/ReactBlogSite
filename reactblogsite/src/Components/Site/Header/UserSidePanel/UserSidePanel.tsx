036d6f085595409cb79578e39414caac944ca492
Tastan Kodersen <andmarknu@hotmail.com>
2024-03-15 07:02:36 +0100

    15.03.2024 07:02

--- reactblogsite/src/Components/Site/Header/UserSidePanel/UserSidePanel.tsx
+++ reactblogsite/src/Components/Site/Header/UserSidePanel/UserSidePanel.tsx
@@ -1,7 +1,8 @@
 import './UserSidePanel.scss'
 import { useUserContext } from '../../../../Hooks/Context/useContext/useUserContext'
 export const UserSidePanel = () => {
-
+    const openBurgerMenu = () => {}
+    const closeBurgerMenu = () => {}
     return (
         <div>
             <div id="openHeaderburgerMenu" className="openHeaderburgerMenu" onClick={()=>{}}>☰</div>
